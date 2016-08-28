4765367649f44882c212b47aad67af0b3fcfcfbd
ferym26 <ferym26@gmail.com>
2016-08-28 20:27:44 +0300

    version 1.3

--- app/js/common.js
+++ app/js/common.js
@@ -1,134 +1,128 @@
-(function($) {
-	$(document).ready(function() {
+$(document).ready(function() {
 
-
-
-
-
-		//Слайдер
-		$(".owl-carousel").owlCarousel({
-			loop: true,
-			margin: 10,
-			nav: true,
-			dots: true,
-			navText: [],
-			responsiveClass: true,
-			touchDrag: false,
-			responsive:{
-				0:{
-						items:1
-				},
-				768:{
-						items:2
-				},
-				1200:{
-						items:3
-				}
+	//Слайдер
+	$(".owl-carousel").owlCarousel({
+		loop: true,
+		margin: 10,
+		nav: true,
+		dots: true,
+		navText: [],
+		responsiveClass: true,
+		touchDrag: false,
+		responsive:{
+			0:{
+					items:1
+			},
+			768:{
+					items:2
+			},
+			1200:{
+					items:3
 			}
-		});
-
-		//Tabs
-		$('ul.tabs__caption').on('click', 'li:not(.active)', function() {
-			$(this)
-				.addClass('active').siblings().removeClass('active')
-				.closest('.tabs').find('.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
-		});
-
-
-		//Animate.scss + WayPoints JS plugin with settings in sass
-		$.fn.animated = function(animName) {
-			$(this).each(function() {
-				var ths = $(this);			
-				$(this).css("opacity", "0").addClass("animated").waypoint(function(dir) {
-					ths.addClass(animName);
-				}, {
-				 offset: '80%'
-				});
-
-			});	 		
-		};
-		//Init animation
-		// .item - target item
-		// animation-name - class from _animation.sass
-		$(".item").animated("animation-name");
-		
-
-		//Переход по якорям
-		//Функция плавности (easing) http://easings.net/ru#
-		$("a.menuLink").click(function() {
-			$("html, body").animate({
-				scrollTop: $($(this).attr("href")).offset().top + "px"
-			}, {
-				duration: 1000,
-				easing: "easeOutCubic"
-			});
-			return false;
-		});
-
-
-		//переключатель active
-		$(".class").click(function() {
-			$(".class").removeClass("active");
-			$(this).addClass("active")
-		});
-
-
-		//прокрутка страницы вверх
-		$('#js_btnUp').click(function() {
-			$('html, body').animate({
-				scrollTop: 0
-			}, {
-				duration: 1000,
-				easing: "easeOutQuart"
-				});
-			return false;
-		});
-
-
-		//Цели для Яндекс.Метрики и Google Analytics
-		$(".count_element").on("click", (function() {
-			ga("send", "event", "goal", "goal");
-			yaCounterXXXXXXXX.reachGoal("goal");
-			return true;
-		}));
-
-
-		//SVG Fallback
-		if(!Modernizr.svg) {
-			$("img[src*='svg']").attr("src", function() {
-				return $(this).attr("src").replace(".svg", ".png");
-			});
-		};
-
-
-		//E-mail Ajax Send
-		$("#form1").submit(function() { //Change
-			var th = $(this);
-			$.ajax({
-				type: "POST",
-				url: "mail.php", //Change
-				data: th.serialize()
-			}).done(function() {
-				alert("Thank you!");
-				setTimeout(function() {
-					// Done Functions
-					th.trigger("reset");
-				}, 1000);
-			});
-			return false;
-		});
-
-
-		//Chrome Smooth Scroll
-		try {
-			$.browserSelector();
-			if($("html").hasClass("chrome")) {
-				$.smoothScroll();
-			}
-		} catch(err) {
-
-		};
-
-
+		}
 	});
-})(jQuery);
+
+	//Tabs
+	$('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+		$(this)
+			.addClass('active').siblings().removeClass('active')
+			.closest('.tabs').find('.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+	});
+
+
+	//Animate.scss + WayPoints JS plugin with settings in sass
+	$.fn.animated = function(animName) {
+		$(this).each(function() {
+			var ths = $(this);			
+			$(this).css("opacity", "0").addClass("animated").waypoint(function(dir) {
+				ths.addClass(animName);
+			}, {
+			 offset: '80%'
+			});
+
+		});	 		
+	};
+	//Init animation
+	// .item - target item
+	// animation-name - class from _animation.sass
+	$(".item").animated("animation-name");
+	
+
+	//Переход по якорям
+	//Функция плавности (easing) http://easings.net/ru#
+	$("a.menuLink").click(function() {
+		$("html, body").animate({
+			scrollTop: $($(this).attr("href")).offset().top + "px"
+		}, {
+			duration: 1000,
+			easing: "easeOutCubic"
+		});
+		return false;
+	});
+
+
+	//переключатель active
+	$(".class").click(function() {
+		$(".class").removeClass("active");
+		$(this).addClass("active")
+	});
+
+
+	//прокрутка страницы вверх
+	$('#js_btnUp').click(function() {
+		$('html, body').animate({
+			scrollTop: 0
+		}, {
+			duration: 1000,
+			easing: "easeOutQuart"
+			});
+		return false;
+	});
+
+
+	//Цели для Яндекс.Метрики и Google Analytics
+	$(".count_element").on("click", (function() {
+		ga("send", "event", "goal", "goal");
+		yaCounterXXXXXXXX.reachGoal("goal");
+		return true;
+	}));
+
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+
+	//E-mail Ajax Send
+	$("#form1").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,10 @@ var	gulp           = require('gulp'),
 		pngquant       = require('imagemin-pngquant'),
 		newer          = require('gulp-newer'),//отдает в поток только обновлнные файлы
 
-		del            = require('del');
+		del            = require('del'),
+
+		gutil          = require('gulp-util'),
+		ftp            = require('vinyl-ftp');
 
 
 
@@ -32,12 +35,12 @@ var	gulp           = require('gulp'),
 
 
 
-gulp.task('serve', ['styles', 'vendors', 'pug', 'imgmin'], function() {
+gulp.task('serve', ['styles', 'vendors', 'pug'], function() {
 		browserSync.init({
 				server: {
-						baseDir: "./app"
+					baseDir: "./app"
 				},
-				port: 3003,
+				port: 3000,
 				notify: false,
 				reloadDelay: 300
 		});
@@ -47,82 +50,147 @@ gulp.task('serve', ['styles', 'vendors', 'pug', 'imgmin'], function() {
 
 
 gulp.task('styles', function () {
-		gulp.src('frontend/sass/*.{scss,sass}')
-				//.pipe(sourcemaps.init())
-				.pipe(sass({includePaths: require('node-bourbon').includePaths}))
-				.on('error', notify.onError({
-						title: 'SASS error'
-				}))
-				.pipe(rename({suffix: '.min', prefix : ''}))
-				.pipe(autoprefixer({browsers: ['last 15 versions'], cascade: false}))
-				.pipe(cleanCSS())
-				//.pipe(sourcemaps.write('.'))
-				.pipe(gulp.dest('app/css'))
-				.pipe(browserSync.stream());
+	gulp.src('frontend/sass/*.{scss,sass}')
+		.pipe(sourcemaps.init())
+		.pipe(sass({includePaths: require('node-bourbon').includePaths}))
+		.on('error', notify.onError({
+			title: 'SASS error'
+		}))
+		.pipe(rename({suffix: '.min', prefix : ''}))
+		.pipe(autoprefixer({browsers: ['last 15 versions'], cascade: false}))
+		.pipe(cleanCSS())
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.stream());
 });
 
 
 
 gulp.task('pug', function build() {
-		gulp.src('frontend/pug/*.pug')
-				.pipe(plumber())
-				.pipe(pug({pretty: true, basedir: __dirname + '/frontend/pug'}))
-				.on('error', notify.onError({
-						title: 'PUG error'
-				}))
-				.pipe(gulp.dest('app'))
-				.pipe(browserSync.stream());
+	gulp.src('frontend/pug/*.pug')
+		.pipe(plumber())
+		.pipe(pug({pretty: true, basedir: __dirname + '/frontend/pug'}))
+		.on('error', notify.onError({
+			title: 'PUG error'
+		}))
+		.pipe(gulp.dest('app'))
+		.pipe(browserSync.stream());
 });
 
 
 
 gulp.task('imgmin', function () {
-		return gulp.src('frontend/img/*.{png,jpg,jpeg,svg}')
-				.pipe(newer('app/img/'))
-				.pipe(imagemin({
-						progressive: true,
-						svgoPlugins: [{removeViewBox: false}],
-						use: [pngquant()]
-				}))
-				.pipe(gulp.dest('app/img/'));
+	return gulp.src('app/img/**/*')		
+		.pipe(imagemin({
+			interlaced: true,
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		}))
+		.pipe(gulp.dest('dist/img/'));
 });
 
 
 
 gulp.task('bower', function(){
-		var filterJS = filter('**/*.js', { restore: true });
-		return gulp.src('./bower.json')
-				.pipe(mainBowerFiles( ))
-				.pipe(filterJS)
-				.pipe(concat('bower_components.js'))
-				//.pipe(filterJS.restore) //оставляет только js когда off
-				.pipe(gulp.dest('app/js/'));
+	var filterJS = filter('**/*.js', { restore: true });
+	return gulp.src('./bower.json')
+		.pipe(mainBowerFiles( ))
+		.pipe(filterJS)
+		.pipe(concat('bower_components.js'))
+		//.pipe(filterJS.restore) //оставляет только js когда off
+		.pipe(gulp.dest('app/js/'));
 });
 
 
 
 gulp.task('vendors', ['bower'], function () {
-		return gulp.src(['app/js/bower_components.js', 'app/libs/**/*.js'])
-				//.pipe(sourcemaps.init())
-				.pipe(concat('vendors.js'))
-				//.pipe(uglify()) //off for speed
-				//.pipe(sourcemaps.write())
-				.pipe(gulp.dest('app/js/'))
+	return gulp.src(['app/js/bower_components.js', 'app/libs/**/*.js'])
+		//.pipe(sourcemaps.init())
+		.pipe(concat('vendors.js'))		
+		//.pipe(sourcemaps.write())
+		.pipe(gulp.dest('app/js/'))
 });
 
 
 
 gulp.task('watch', function () {
-		gulp.watch('frontend/sass/**/*.{scss,sass}', ['styles']);
-		gulp.watch('frontend/pug/**/*.pug', ['pug']);
+	gulp.watch('frontend/sass/**/*.{scss,sass}', ['styles']);
+	gulp.watch('frontend/pug/**/*.pug', ['pug']);
 
-		gulp.watch('app/libs/**/*.js', ['vendors']);
-		gulp.watch('bower_components/**/*.js', ['vendors']);
+	gulp.watch('app/libs/**/*.js', ['vendors']);
+	gulp.watch('bower_components/**/*.js', ['vendors']);	
 
-		gulp.watch('frontend/img/**/*.{png,jpg,jpeg,svg}', ['imgmin']);
+	gulp.watch('app/js/*.js').on("change", browserSync.reload);
+	gulp.watch('app/*.html').on('change', browserSync.reload);		
+});
 
-		gulp.watch('app/js/*.js').on("change", browserSync.reload);
-		gulp.watch('app/*.html').on('change', browserSync.reload);		
+
+/*
+gulp.task('buildhtml', function() {
+  gulp.src(['app/*.html'])
+    .pipe(fileinclude({
+      prefix: '@@'
+    }))
+    .pipe(gulpRemoveHtml())
+    .pipe(gulp.dest('dist/'));
+});
+*/
+
+
+gulp.task('removedist', function() {
+	return del.sync('dist');
+});
+
+
+
+gulp.task('build', ['removedist', 'imgmin', 'styles', 'vendors'], function() {
+
+	var buildHtml = gulp.src(['app/*.html'])
+		.pipe(gulp.dest('dist'));
+
+	var buildCss = gulp.src([
+		'app/css/fonts.min.css',
+		'app/css/main.min.css'])
+		.pipe(gulp.dest('dist/css'));
+
+	var buildJsVendors = gulp.src('app/js/vendors.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/js'));
+
+	var buildJsCommon = gulp.src('app/js/common.js')		
+		.pipe(gulp.dest('dist/js'));
+
+	var buildFonts = gulp.src('app/fonts/**/*')
+		.pipe(gulp.dest('dist/fonts'));				
+
+	var buildHtaccess = gulp.src(['app/rename.htaccess-rename'])
+		.pipe(rename(".htaccess"))
+		.pipe(gulp.dest('dist'));
+
+	var buildFiles = gulp.src(['app/*.*', '!app/index.html', '!app/rename.htaccess-rename'])
+		.pipe(gulp.dest('dist'));
+});
+
+
+
+gulp.task('deploy', function() {
+
+	var conn = ftp.create({
+		host:      'hostname.com',
+		user:      'username',
+		password:  'userpassword',
+		parallel:  10,
+		log: gutil.log
+	});
+
+	var globs = [
+	'dist/**',
+	'dist/.htaccess',
+	];
+	return gulp.src(globs, {buffer: false})
+		.pipe(conn.dest('/path/to/folder/on/server'));
+
 });
 
 
